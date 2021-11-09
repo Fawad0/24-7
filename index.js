3228a5c1dda4fef87d7ac3e35b2e7a00ca884040
Fawad0 <88716984+Fawad0@users.noreply.github.com>
2021-11-10 04:35:04 +0500

    Update index.js

--- index.js
+++ index.js
@@ -28,8 +28,9 @@ bot.loadPlugin(cmd)
 
 
 bot.on('login',function(){
-	console.log("/register Fawad Fawad")
+	console.log("Logged In")
 	bot.chat("hello");
+	
 });
 
 bot.on('time', function(time) {
